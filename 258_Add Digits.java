4865a63df05ea7f518220838e0c55103bd7bf91e
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-08-27 20:48:24 +0800

    Create 258_Add Digits.java

---
+++ 258_Add Digits.java
@@ -0,0 +1,23 @@
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// For example:
+
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+//point: Could you do it without any loop/recursion in O(1) runtime?
+
+public class Solution {
+    public int addDigits(int num) {
+        int result=10;
+        while(result>9){
+            result = 0;
+            while(num>=10){
+                result += num % 10;
+                num = num/10;
+            }
+            result += num;
+            num = result;
+        }
+        return result;
+    }
+}
