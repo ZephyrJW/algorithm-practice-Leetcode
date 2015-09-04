218fee16620b2d18bc1e6b441ce55dc24855b2fb
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-09-04 13:12:50 +0800

    Update 13_Roman to integer.java

--- 13_Roman to integer.java
+++ 13_Roman to integer.java
@@ -38,3 +38,28 @@ public class Solution {
       }
   }
 }
+
+
+// another solution, inspiring, from the end of the roman numeber to transfer & uses a HashMap
+
+public class Solution {
+    public int romanToInt(String s) {
+        if (s==null || s.isEmpty()) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int prev = 0, res=0;
+        for (int i=s.length()-1; i>=0; i--) {
+            int cur = map.get(s.charAt(i));
+            if (cur<prev) res-=cur;
+            else res+=cur;
+            prev = cur;
+        }
+        return res;
+    }
+}
