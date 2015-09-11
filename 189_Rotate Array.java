de666105253425b06f224d897c0c8fb2c11501cc
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-09-11 20:42:02 +0800

    Update 189_Rotate Array.java

--- 189_Rotate Array.java
+++ 189_Rotate Array.java
@@ -6,16 +6,34 @@
 // Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 
 
-
+//1.
 public class Solution {
     public void rotate(int[] nums, int k) {
         if (nums == null || nums.length < 2) {
             return;
         }
-        k = k % nums.length;   //can't understand the '%', how can k > n ?
+        k = k % nums.length;   //can't understand the '%', how can k > n ?  ans: rotate k=sums.length return the original sums
+        //then k-sums.length should be the k actually
         int[] tmp = new int[k];
         System.arraycopy(nums, nums.length - k, tmp, 0, k);
         System.arraycopy(nums, 0, nums, k, nums.length - k);
         System.arraycopy(tmp, 0, nums, 0, k);
     }
+}
+//System.arraycopy: (array to copy from, start point, array copy to, start point, end point)
+
+//2. @oybin1989 doubling the array
+public void rotate(int[] nums, int k) {
+    int length=nums.length;
+    int rk=k%length;
+    int [] copy=new int[length*2];
+    for(int i=0;i<2*length;i++){
+        copy[i]=nums[i%length];
+    }
+    for(int t=length-rk,i=0;t<2*length-rk;t++,i++)
+    {
+        nums[i]=copy[t];
+
+    } 
+
 }
