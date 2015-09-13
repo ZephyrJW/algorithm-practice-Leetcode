1f7235b79dbc36fd596570e1fb7939cd581a8c6f
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-09-13 15:19:59 +0800

    Update 191_Number of 1 Bits.java

--- 191_Number of 1 Bits.java
+++ 191_Number of 1 Bits.java
@@ -2,7 +2,7 @@
 
 // For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
 
-//inspired by @ alderith and @Todd2
+//inspired by @ alderith and @Todd2 , difference : actually this is to find 1 and count
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
@@ -15,7 +15,7 @@ public class Solution {
     }
 }
 
-//you need to read a java book , by @ hello_today_
+//you need to read a java book , by @ hello_today_  --however,this is to go through the 1-or-0
 public int hammingWeight(int n) {
     int x = 0;
     while (n != 0) {
@@ -24,3 +24,12 @@ public int hammingWeight(int n) {
     }
     return x;
 }
+
+//addtional knowledge : 
+//1.>> 右移运算符 >>>无符号右移 移动n位，表示除以2的n次方
+
+// 2.对于：&   -- >  不管怎样，都会执行"&"符号左右两边的程序
+//   对于：&& -- >  只有当符号"&&"左边程序为真(true)后，才会执行符号"&&"右边的程序。
+
+//3. & 与运算符   | 或运算符  ~ 非运算符   ^ 异或运算符：两个操作数的位中，相同则结果为0，不同则结果为1。
+
