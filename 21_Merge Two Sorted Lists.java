45d15dd4293ff970b00f2d31d6e50638e1344bf7
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-09-16 11:16:14 +0800

    Create 21_Merge Two Sorted Lists.java

---
+++ 21_Merge Two Sorted Lists.java
@@ -0,0 +1,60 @@
+//Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ //recursive
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
+
+// a more straightforward version
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        
+        // merge
+        ListNode beforeHead = new ListNode(0);
+        ListNode temp = new ListNode(0);
+        beforeHead.next = temp;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        // merge remain
+        while (l1 != null) {
+            temp.next = l1;
+            temp = temp.next;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            temp.next = l2;
+            temp = temp.next;
+            l2 = l2.next;
+        }
+        return beforeHead.next.next;
+    }
+}
