4f945729cbb428a950f588954c494477d381aa83
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-09-26 14:19:22 +0800

    Update 136_Single number.java

--- 136_Single number.java
+++ 136_Single number.java
@@ -14,3 +14,31 @@ public class Solution {
         return nums[i];
     }
 }
+
+//@ Nkeys  BOTH EXPLANATION AND SOLUTION
+    // we use bitwise XOR to solve this problem :
+    
+    // first , we have to know the bitwise XOR in java
+    
+    // 0 ^ N = N
+    // N ^ N = 0
+    // So..... if N is the single number
+    
+    // N1 ^ N1 ^ N2 ^ N2 ^..............^ Nx ^ Nx ^ N
+    
+    // = (N1^N1) ^ (N2^N2) ^..............^ (Nx^Nx) ^ N
+    
+    // = 0 ^ 0 ^ ..........^ 0 ^ N
+    
+    // = N
+
+public int singleNumber(int[] nums) {
+    int ans =0;
+
+    int len = nums.length;
+    for(int i=0;i!=len;i++)
+        ans ^= nums[i];
+
+    return ans;
+
+}
