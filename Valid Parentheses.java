2936b383fb6f2d519305e36837172b5a38ddea7b
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-10-18 08:58:25 +0800

    Update Valid Parentheses.java

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -3,13 +3,34 @@
 // The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 //however, ([]) is correct 
 
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        // Iterate through string until empty
+        for(int i = 0; i<s.length(); i++) {
+            // Push any open parentheses onto stack
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            // Check stack for corresponding closing parentheses, false if not valid
+            else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else
+                return false;
+        }
+        // return true if no open parentheses left in stack
+        return stack.empty();
+    }
+}
 
 
 
 
 
-
-//previous solution, misunderstood the meaning of the problem.
+//previous solution, misunderstood the meaning of the problem. It may have (){}[], 
 public class Solution {
     public boolean isValid(String s) {
         char[] test = s.toCharArray();
