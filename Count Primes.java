40cebf269c78df348acb952a0f3cef7f2960ae4c
Jiawei Zhao <jiawei_zhao@outlook.com>
2015-12-02 16:18:15 +0800

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,24 @@
+// Description:
+
+// Count the number of prime numbers less than a non-negative number, n.
+
+//it's called SIEVE OF ERATOSTHENES, kind of simple way of counting primes
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for(int i =2; i<n;i++){
+            isPrime[i] = true;
+        }
+        for(int i=2; i*i<n; i++){
+            if(!isPrime[i]) continue;
+            for(int j = i*i; j<n; j+=i){
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(isPrime[i]) count++;
+        }
+        return count;
+    }
+}
