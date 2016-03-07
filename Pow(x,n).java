f874205221adf1338a8ef28422653f744c277f6b
Jiawei Zhao <jiawei_zhao@outlook.com>
2016-03-07 23:18:27 +0800

    Create Pow(x,n).java

---
+++ Pow(x,n).java
@@ -0,0 +1,23 @@
+// Implement pow(x, n).
+ 
+ //see this as blow:
+ //2^5 = 2^2 * 2^2 * 2. that's how this work this problem out 
+ //divide and conquer
+ public class Solution {
+        public double myPow(double x, int m) {
+            double temp=x;
+            if(m==0)
+            return 1;
+            temp=myPow(x,m/2);
+            if(m%2==0)
+            return temp*temp;
+            else 
+            {
+            if(m > 0)
+                return x*temp*temp;
+            else
+                return (temp*temp)/x;
+            }
+
+    }
+}
