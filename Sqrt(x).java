601729dc0c604d7d56f584d8b687416ac84562d4
Jiawei Zhao <jiawei_zhao@outlook.com>
2016-03-08 13:05:09 +0800

    Update Sqrt(x).java

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -9,7 +9,7 @@ public class Solution {
         if(x<=1) return x;
         int low = 1, high = x;
         while(low<=high){
-            int mid = low + (high-low)/2;
+            int mid = low + (high-low)/2; //or int mid = (low+high) >>> 1;
             if(mid == x/mid){
                 return mid;
             }else if(mid > x/mid){
@@ -21,3 +21,7 @@ public class Solution {
         return high;
     }
 }
+
+//review: 1. (low+high)/2 may overflow.
+//        2.  uses >>> because it's clear that low+high must be non-negative. >> cannot guarantee no overflow happens
+//使用(low + high) >>> 1求平均值仅适用于结果保证不是负数的情况，但好处是即使两个数的和int溢出也能求出正确的结果
